0089a8b4ade87c43011dc4296b0cb6a8ac821d0c
Mauricio-PC\Mauricio <balvarez@yavirac.edu.ec>
2018-06-28 00:31:28 -0500

    Calculo de numero de la IP a numero Binario

---
+++ app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  num1: Text;
+  resultado: Text;
+  array=Array[9];
+
+
+  public numeros = [
+    '1','2','3'
+  ];
+
+  if () {
+    
+  }
+ 
+}
